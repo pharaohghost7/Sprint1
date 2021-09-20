c18ec09488706b5c5663157f90bc1fb088148083
Andres Betancourt <andresbetancourtrc@gmail.com>
2021-09-20 12:01:41 -0500

    Cambios en la base de datos

--- Backend/BD.js
+++ Backend/BD.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-
+            //mongod para repositorio locales
 const URI = 'mongodb+srv://andresb:12345ab@cluster0.cqqrc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-                //mongod
+             
              // localhost:27017  //localhost:27017
 mongoose.connect(URI)
     .then(db => console.log('Db connect'))
